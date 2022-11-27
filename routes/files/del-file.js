f6da2d24fdf7c01680949fa7bcf546ea7bf66862
alex <tte577es7@yandex.ru>
2022-11-27 13:59:01 +0300

    less 36 part 2

--- routes/files/del-file.js
+++ routes/files/del-file.js
@@ -17,7 +17,26 @@ module.exports = (app) => {
      * Метод: POST
      */
      app.post('/file/del', fileFromForm, function(req, res){
-
+        //Получим название файла, переданное из формы
+        const fileName = req.body.name
+        const resFromAPI = {}
+        //Применяем метод unlink для удаления
+        fs.unlink(`./uploads/${fileName}`, function(err){
+            if(err){
+                resFromAPI.status = 500;
+                resFromAPI.message = 'Файл не удалось удалить из папки'
+                resFromAPI.error = err
+                res.send(
+                    JSON.stringify(resFromAPI)
+                )
+            }else{
+                resFromAPI.status = 200;
+                resFromAPI.message = 'Файл успешно удален из папки'
+                res.send(
+                    JSON.stringify(resFromAPI)
+                )
+            }
+        })
      })
 
 
@@ -37,7 +56,7 @@ module.exports = (app) => {
                 Тестовая форма, для маршрута - /file/del
                 </h1>
                 <form enctype="multipart/form-data"  action='/file/del' method='post'>
-                    <input type='text' placeholder='Введите название файла'/>
+                    <input type='text' name='name' placeholder='Введите название файла'/>
                     <input type='submit' value='Удалить' />
                 </form>
             `
